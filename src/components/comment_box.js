5ccf2f651dfb230a21d94b191a6f7cbf737af95a
Tin Chu <mchu.tinho@gmail.com>
2018-01-26 11:44:27 -0600

    first test on React components

---
+++ src/components/comment_box.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CommentBox extends Component {
+  render() {
+    return (
+      <div>
+        <textarea></textarea>
+        <button></button>
+      </div>
+    );
+  }
+}
+export default CommentBox;
--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,21 @@
-import { renderComponent , expect } from '../test_helper';
+import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
+// // use `describe` to group together similar tests
+// describe('App')
+//
+// // Use `it` to test a single attr of the target
+// it('shows the correct text')
+//
+// // Use `expect` to make an `assertion` about a target
+// expect
+describe('App', () => {
+  it('shows the correct text', () => {
+    // create an instance of app
+    const component = renderComponent(App);
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
+    // user `expect` to make an `assertion` about target
+    expect(component).to.contain('React simple starter');
 
-  it('renders something', () => {
-    expect(component).to.exist;
   });
 });
